ba156e302ffb297b533a27bfc73ebdd3941593c9
ru-461 <48552543+ru-461@users.noreply.github.com>
2024-01-09 21:38:08 +0900

    chore: Add expo-router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,15 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
