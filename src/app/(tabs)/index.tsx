c48fab2c70415e8aace10492bb72d0cb83528fa6
ru-461 <48552543+ru-461@users.noreply.github.com>
2024-01-11 22:51:00 +0900

    update: Add FlatList

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+import { FlatList } from 'react-native-gesture-handler';
+import useSWR from 'swr';
+
+import ArticleCard from '../../components/ArticleCard';
+import { Article } from '../../types';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 const TrendingTabScreen = () => {
+  const { data, error, isLoading } = useSWR(
+    'https://zenn.dev/api/articles',
+    fetcher,
+  );
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text>failed to load</Text>
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  const articles = data.articles as Array<Article>;
+
   return (
     <View style={styles.container}>
-      <Text>Trending tab screen</Text>
+      <FlatList
+        data={articles}
+        renderItem={article => (
+          <ArticleCard article={article.item} key={article.item.id} />
+        )}
+      />
     </View>
   );
 };
@@ -12,7 +48,7 @@ export default TrendingTabScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fffffe',
+    backgroundColor: '#ECF5FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
