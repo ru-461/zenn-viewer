bad49202b68a2b341604ed496974aed37117f1a1
ru-461 <48552543+ru-461@users.noreply.github.com>
2024-01-22 23:12:38 +0900

    update: Create search box

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,11 +1,40 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 const SearchScreen = () => {
+  const [value, setValue] = useState('');
+  const textInputRef = useRef(null);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      // フォーカス
+      if (textInputRef.current) {
+        textInputRef.current.focus();
+      }
+    }, 500);
+
+    // クリーンアップ
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  const handleChangeText = (text: string) => {
+    setValue(text);
+  };
+
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: 'Search', headerBackTitle: 'Back' }} />
-      <Text>Search</Text>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.textInput}
+          value={value}
+          ref={textInputRef}
+          placeholder="Search keyword..."
+          placeholderTextColor="#000"
+          onChangeText={(value) => handleChangeText(value)}
+        />
+      </View>
     </View>
   );
 };
@@ -14,8 +43,23 @@ export default SearchScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fffffe',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fffffe',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+  },
+  textInput: {
+    flex: 1,
+    borderWidth: 0.5,
+    borderRadius: 16,
+    height: 48,
+    margin: 24,
+    padding: 8,
+  },
+  contentsLayout: {
+    flex: 1,
+    marginTop: 12,
+    marginHorizontal: 16,
   },
 });
