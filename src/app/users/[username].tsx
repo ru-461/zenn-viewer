19f59cc053e8ade0735f79649b5412916357ccb3
ru-461 <48552543+ru-461@users.noreply.github.com>
2024-02-11 01:00:52 +0900

    update: Fetch user articles in user page

--- src/app/users/[username].tsx
+++ src/app/users/[username].tsx
@@ -1,27 +1,79 @@
-import TabBarIcon from '@/src/components/TabbarIcon';
+import ArticleCard from '@/src/components/ArticleCard';
+import { Article } from '@/src/types';
+import { FlashList } from '@shopify/flash-list';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import {
+  ActivityIndicator,
+  RefreshControl,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import useSWR from 'swr';
 
-const Page = () => {
+const userScreen = () => {
   const { username } = useLocalSearchParams();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const { data, error, isLoading, mutate } = useSWR(
+    `https://zenn.dev/api/articles?username=${username}&order=latest`,
+  );
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    mutate();
+    setRefreshing(false);
+  };
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text>Failed to load.</Text>
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  // Type
+  const articles = data.articles as Array<Article>;
 
   return (
-    <View style={styles.container}>
+    <View style={styles.listContainer}>
       <Stack.Screen
         options={{
-          title: `${username}`,
+          title: 'aaa',
           headerTintColor: '#fff',
           headerTitleStyle: {
             fontWeight: 'bold',
           },
         }}
       />
-      <TabBarIcon color="red" name="code" />
+      <FlashList
+        data={articles}
+        onRefresh={onRefresh}
+        refreshing={refreshing}
+        estimatedItemSize={200}
+        keyExtractor={(articles) => `${articles.id}`}
+        refreshControl={
+          <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+        }
+        renderItem={(article) => (
+          <ArticleCard article={article.item} key={article.item.id} />
+        )}
+      />
     </View>
   );
 };
 
-export default Page;
+export default userScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -29,5 +81,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fffffe',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
   },
 });
